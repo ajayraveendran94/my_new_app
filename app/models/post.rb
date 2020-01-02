1495b8c5dfe2f6534a5c2029aee50e31e1a6e544
ajayraveendran94 <ajayraveendran94@gmail.com>
2020-01-02 10:24:04 +0530

    comment section completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+	has_many :comments
 	validates :name, presence: true, length: {minimum:5}
 	validates :mobile, presence: true, length: {minimum:10}
 	validates :post, presence: true, length: {minimum:10}
